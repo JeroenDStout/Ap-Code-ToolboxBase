8bca59f1c7904145cdcbe11c484d9feed4a6e201
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-12 19:56:16 +0200

    Add todo note

--- Pubc/Interface Messages.h
+++ Pubc/Interface Messages.h
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// TODO: Replace the complicated recursive calling child types with a
+// single map which calls proper functions
+
 #pragma once
 
 #include <map>
